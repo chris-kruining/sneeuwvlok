eff0c1ad83a5004c8f0058ad4faa8971d8bdb9af
Chris Kruining <chris@kruining.eu>
2024-07-23 18:43:28 +0200

    .

---
+++ modules/desktop/browsers/default.nix
@@ -0,0 +1,26 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  cfg = config.modules.desktop.browsers;
+in {
+  options.modules.desktop.browsers = let
+    inherit (lib.options) mkOption;
+    inherit (lib.types) nullOr str;
+  in {
+    default = mkOption {
+      type = nullOr str;
+      default = null;
+      description = "Default system browser";
+      example = "firefox";
+    };
+  };
+
+  config = mkIf (cfg.default != null) {
+    home.sessionVariables.BROWSER = cfg.default;
+  };
+}
