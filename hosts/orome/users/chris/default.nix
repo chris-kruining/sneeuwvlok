84f13b86489020a258121f72532499a6bb0be6c7
Chris Kruining <chris@kruining.eu>
2025-03-19 10:56:44 +0000

    Update default.nix

--- hosts/orome/users/chris/default.nix
+++ hosts/orome/users/chris/default.nix
@@ -8,6 +8,42 @@
     polarity = "dark";
   };
 
+  develop = {
+    rust.enable = true;
+    js.enable = true;
+    dotnet.enable = true;
+  };
+
+  desktop = {
+    plasma.enable = true;
+
+    applications = {
+      communication.enable = true;
+      email.enable = true;
+      office.enable = true;
+    };
+
+    terminal = {
+      default = "ghostty";
+      ghostty.enable = true;
+    };
+
+    editors = {
+      default = "zed";
+      vscodium.enable = true;
+      zed.enable = true;
+      nvim.enable = true;
+      nano.enable = true;
+    };
+
+    browsers = {
+      default = "chromium";
+      firefox.enable = true;
+      chrome.enable = true;
+    };
+  };
+
+
   shell = {
     default = "zsh";
   };
