043eded2497049b3592b2efef2f135a9dfa40346
Chris Kruining <c.kruining@4dotnet.nl>
2025-08-07 14:12:16 +0200

    fix

--- modules/nixos/services/development/forgejo/default.nix
+++ modules/nixos/services/development/forgejo/default.nix
@@ -31,7 +31,26 @@ in
         };
       };
 
-      services.caddy = {
+      gitea-actions-runner = {
+        package = pkgs.forgejo-actions-runner;
+        instances.default = {
+          enable = true;
+          name = "monolith";
+          url = "https://git.kruining.eu";
+          # Obtaining the path to the runner token file may differ
+          # tokenFile should be in format TOKEN=<secret>, since it's EnvironmentFile for systemd
+          tokenFile = config.age.secrets.forgejo-runner-token.path;
+          labels = [
+            "ubuntu-latest:docker://node:16-bullseye"
+            "ubuntu-22.04:docker://node:16-bullseye"
+            "ubuntu-20.04:docker://node:16-bullseye"
+            "ubuntu-18.04:docker://node:16-buster"
+            "native:host"
+          ];
+        };
+      };
+
+      caddy = {
         enable = true;
         virtualHosts = {
           "git.kruining.eu".extraConfig = ''
