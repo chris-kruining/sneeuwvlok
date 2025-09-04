522041cbaed64b9cc9699e7feb82c2eceea81e6f
Chris Kruining <c.kruining@4dotnet.nl>
2025-09-04 15:47:37 +0200

    waaaaaaggh

--- modules/nixos/services/development/forgejo/runners/default.nix
+++ modules/nixos/services/development/forgejo/runners/default.nix
@@ -11,7 +11,7 @@ dockerTools.buildImage {
   copyToRoot = buildEnv {
     name = "image-root";
     pathsToLink = [ "/bin" ];
-    paths = with pkgs_linux [
+    paths = with pkgs_linux; [
       coreutils
       u-root-cmds
       bash
