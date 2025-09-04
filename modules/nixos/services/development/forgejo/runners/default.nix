09a5df6253e3dd5556800388e34f64b9ae234ba3
Chris Kruining <c.kruining@4dotnet.nl>
2025-09-04 14:53:50 +0200

    fix?

--- modules/nixos/services/development/forgejo/runners/default.nix
+++ modules/nixos/services/development/forgejo/runners/default.nix
@@ -3,11 +3,35 @@
   pkgs_linux ? import <nixpkgs> { system = "x86_64-linux"; },
 }:
 
-pkgs.dockerTools.buildImage {
+with pkgs; 
+dockerTools.buildImage {
   name = "default";
   tag = "latest";
 
+  contents = [
+    coreutils
+    u-root-cmds
+    bash
+    nix
+    nodejs
+    podman
+  ];
+
+  runAsRoot = ''
+    #!${stdenv.shell}
+    ${dockerTools.shadowSetup}
+    groupadd -r runner
+    useradd -r -g runner -d /data -M runner
+    mkdir /data
+    chown runner:runner /data
+  '';
+
   config = {
-    Cmd = [ "${pkgs_linux.hello}/bin/hello" ];
+    # User = "root";
+    Cmd = [ "${lib.getExe bashInteractive}" ];
+    WorkingDir = "/data";
+    Volumes = {
+      "/data" = {};
+    };
   };
 }
