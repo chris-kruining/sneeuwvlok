6ed8bd861b5074084a67d3bf150cdf732476bf31
Chris Kruining <chris@kruining.eu>
2025-09-14 22:03:45 +0200

    start borg backups

---
+++ modules/nixos/services/backup/borg/default.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, namespace, ... }:
+let
+  inherit (lib) mkIf mkEnableOption;
+
+  cfg = config.${namespace}.services.backup.borg;
+in
+{
+  options.${namespace}.services.backup.borg = {
+    enable = mkEnableOption "Borg Backup";
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      borgbackup.jobs = {
+        media = {
+          paths = "/var/media/test";
+          encryption.mode = "none";
+          environment.BORG_SSH = "ssh -i /home/chris/.ssh/id_ed25519 -4";
+          repo = "ssh://chris@beheer.hazelhof.nl:222/home/chris/backups/media";
+          compression = "auto,zstd";
+          startAt = "daily";
+        };
+      };
+    };
+  };
+}
